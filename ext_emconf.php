73d4c5b8d20caaf13e2725f0ee2126881423d094
Mikkel Olrik <molrik@molrik.dk>
2013-04-27 15:42:33 +0200

    First commit
    tw_rss 2.1.4

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "tw_rssfeeds".
+ *
+ * Auto generated 27-04-2013 13:05
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'TW RSS Feeds',
+	'description' => 'Should handle rss feeeds from 0.91 and 2.0 format.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '2.1.4',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Thorsten WÃ¼st',
+	'author_email' => 't.wuest@wuest-media.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"0788";s:12:"ext_icon.gif";s:4:"9692";s:17:"ext_localconf.php";s:4:"ac99";s:14:"ext_tables.php";s:4:"0508";s:13:"locallang.php";s:4:"6c12";s:16:"locallang_db.php";s:4:"9031";s:10:"README.txt";s:4:"f734";s:15:"tw_rss_flex.xml";s:4:"190e";s:14:"doc/manual.sxw";s:4:"a906";s:19:"doc/wizard_form.dat";s:4:"0efd";s:20:"doc/wizard_form.html";s:4:"f738";s:14:"pi1/ce_wiz.gif";s:4:"72e5";s:31:"pi1/class.tx_twrssfeeds_pi1.php";s:4:"ba49";s:39:"pi1/class.tx_twrssfeeds_pi1_wizicon.php";s:4:"80ba";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.php";s:4:"261a";s:24:"pi1/static/editorcfg.txt";s:4:"0a25";s:20:"pi1/static/setup.txt";s:4:"566e";}',
+);
+
+?>
